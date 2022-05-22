ad16b91e2385cd3a77e3c677fe0f6864d36d8604
mosquito0076 <hsmk0076@naver.com>
2022-05-22 23:16:13 +0900

    final

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const bgImg = document.createElement("img");
+
+bgImg.src = "https://source.unsplash.com/featured/?galaxy";
+
+document.body.appendChild(bgImg);
+
+bgImg.setAttribute('class', 'position-fixed')
