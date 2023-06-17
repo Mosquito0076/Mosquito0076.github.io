815cf24e020a51f51ba12c3aa66cfabbbacc3a31
mosquito0076 <hsmk0076@naver.com>
2023-06-17 16:32:37 +0900

    Create Blog

--- js/background.js
+++
@@ -1,7 +0,0 @@
-const bgImg = document.createElement("img");
-
-bgImg.src = "https://source.unsplash.com/featured/?galaxy";
-
-document.body.appendChild(bgImg);
-
-bgImg.setAttribute('class', 'position-fixed')
